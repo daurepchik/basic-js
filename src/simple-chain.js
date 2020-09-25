c4df5305c7e922a7185bc5efa55a41b6339d1856
Aidenov Dauren <daurenblin5@gmail.com>
2020-09-26 00:17:22 +0500

    feat: implement simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  arr: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.arr.push('(  )');
+    } else {
+      this.arr.push(`( ${value} )`);
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position === undefined || position <= 0 || position > this.arr.length || typeof position !== 'number') {
+      this.arr = []
+      throw new Error();
+    } else {
+      this.arr.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let arrTemp = this.arr
+    this.arr = []
+    return arrTemp.join('~~')
   }
 };
 
