7dbcd4997d74d27436bd5eaa169824249863026f
Aidenov Dauren <daurenblin5@gmail.com>
2020-10-10 20:12:41 +0500

    init: implement the task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,67 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag = true) {
+    this.flag = flag
+    this.alphabet = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error;
+
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+
+    let encryptText = [];
+    let keyCounter = 0;
+
+    for (let i in message) {
+
+      let msgLetterPos = this.alphabet.indexOf(message[i])
+
+      if (msgLetterPos === -1) {
+        encryptText.push(message[i]);
+        continue;
+      }
+
+      let keyLetterPos = this.alphabet.indexOf(key[keyCounter % key.length])
+
+      let x = (msgLetterPos + keyLetterPos) % 26;
+      encryptText.push(this.alphabet[x])
+
+      keyCounter++;
+    }
+
+    return this.flag ? encryptText.join('') : encryptText.reverse().join('')
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error;
+
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+
+    let encryptText = [];
+    let keyCounter = 0;
+
+    for (let i in message) {
+
+      let msgLetterPos = this.alphabet.indexOf(message[i])
+
+      if (msgLetterPos === -1) {
+        encryptText.push(message[i]);
+        continue;
+      }
+
+      let keyLetterPos = this.alphabet.indexOf(key[keyCounter % key.length])
+
+      let x = (msgLetterPos - keyLetterPos + 26) % 26;
+      encryptText.push(this.alphabet[x])
+
+      keyCounter++;
+    }
+
+    return this.flag ? encryptText.join('') : encryptText.reverse().join('')
   }
 }
 
